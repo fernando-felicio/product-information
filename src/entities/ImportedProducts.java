4e2797f4d55f9200793e8553ec6201c2a24b0b7f
Fernando Felicio <fernandofelicio.dev@gmail.com>
2024-02-07 18:54:46 -0300

    ImportedProducts Class

---
+++ src/entities/ImportedProducts.java
@@ -0,0 +1,32 @@
+package entities;
+
+public class ImportedProducts extends Product {
+	
+	private Double customsFee;
+	
+	public ImportedProducts() {
+		super();
+	}
+	
+	public ImportedProducts(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	@Override
+	public final String priceTag() {
+		return name +
+				" $ "
+				+ price
+				+" (Customs fee: $ " + customsFee + " )";
+	}
+
+}
